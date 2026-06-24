2a6463985115f7fa3ccb808b129f3a063494dda2
Faisal Moarafur Rasul <155888876+faisalmrasul@users.noreply.github.com>
2026-06-25 03:29:38 +0600

    Initialize utils module with helper functions

---
+++ utils/__init__.py
@@ -0,0 +1,33 @@
+from .helpers import (
+    format_time,
+    calculate_percentage,
+    validate_email,
+    validate_phone,
+    generate_progress_bar,
+    get_learning_recommendations,
+    format_date,
+    truncate_text,
+    load_json_file,
+    save_json_file,
+    get_user_level,
+    generate_study_plan,
+    get_bengali_greeting,
+    translate_to_bengali
+)
+
+__all__ = [
+    'format_time',
+    'calculate_percentage',
+    'validate_email',
+    'validate_phone',
+    'generate_progress_bar',
+    'get_learning_recommendations',
+    'format_date',
+    'truncate_text',
+    'load_json_file',
+    'save_json_file',
+    'get_user_level',
+    'generate_study_plan',
+    'get_bengali_greeting',
+    'translate_to_bengali'
+]
